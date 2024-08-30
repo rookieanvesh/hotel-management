b4695d676d477e2867839ed3f3d3cbaf2014e37a
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-08-30 20:36:05 +0530

    controller testing

---
+++ src/main/java/com/RestIn/HotelBooking/controller/BookingController.java
@@ -0,0 +1,47 @@
+package com.RestIn.HotelBooking.controller;
+
+import com.RestIn.HotelBooking.dto.Response;
+import com.RestIn.HotelBooking.entity.Booking;
+import com.RestIn.HotelBooking.service.interfac.IBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bookings")
+public class BookingController {
+    @Autowired
+    private IBookingService bookingService;
+
+    @PostMapping("/book-room/{roomId}/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")// we only want our admin or user to add a room
+    public ResponseEntity<Response> saveBooking(
+            @PathVariable Long roomId,
+            @PathVariable Long userId,
+            @RequestBody Booking bookingRequest
+    ){
+        Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllBookings(){
+        Response response = bookingService.getAllBookings();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-confirmation-code/{confirmationCode}")
+    public ResponseEntity<Response> getBookingByConfirmationCode(@PathVariable String confirmationCode){
+        Response response = bookingService.findBookingByConfirmationCode(confirmationCode);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/cancel/{bookingId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> cancelBooking(@PathVariable Long bookingId){
+        Response response = bookingService.cancelBooking(bookingId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
